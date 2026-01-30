7008a003a8c28d8f28e160b973b817458d3f4c74
dylanlimyf <dylanlimyf@gmail.com>
2026-01-31 02:24:24 +0800

    Revert "Level - 1. Echo"
    This reverts commit e827155087c96dab0a977e2458b9cd9f5f63d32c.

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,26 +1,9 @@
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Bobby {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
-        String greeting = "Hello! I'm Bobby\n";
+        String hello = "Hello! I'm Bobby\n";
         String doRequest = "What can I do for you?\n";
-        String goodbye = "So you hate me. Okay bye.\n";
-
-        System.out.println(line + greeting + doRequest + line);
-
-        while (true) {
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
-
-            if (Objects.equals(input, "bye")) {
-                System.out.print(goodbye);
-                return;
-            }
-
-            System.out.print(input + "\n" + line);
-
-        }
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + hello + doRequest + line + goodbye + line);
     }
 }
