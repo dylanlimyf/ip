e392f6e982fdaaf354af3aa74f213237c6fc7b50
dylanlimyf <dylanlimyf@gmail.com>
2026-01-30 15:30:21 +0800

    Level-0: rename chatbot to Bobby and add greeting

---
+++ src/main/java/Bobby.java
@@ -0,0 +1,9 @@
+public class Bobby {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String hello = "Hello! I'm Bobby\n";
+        String doRequest = "What can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + hello + doRequest + line + goodbye + line);
+    }
+}
